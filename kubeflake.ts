ae283efdc507dd3c48a7cc2cba213a9c1be7808c
Jiwoong Jung <kanziwoong@gmail.com>
2024-04-15 15:13:45 +0900

    Implement kubeflake

--- kubeflake.ts
+++ kubeflake.ts
@@ -1,1 +1,29 @@
-console.log('Hello, Kubeflake!');
+import * as crypto from 'node:crypto';
+import { hostname } from 'node:os';
+import { Sonyflake } from 'sonyflake-js';
+
+const host = hostname();
+const sum = crypto.createHash('md5').update(host).digest();
+const machineIDBytes = sum.subarray(sum.length - 2); // extract last 2 bytes
+const machineId = BigInt(machineIDBytes.readUInt16BE());
+
+const sf = new Sonyflake({
+  machineId,
+});
+
+export const kubeflake = () => sf.next();
+
+export class Kubeflake {
+  private sf: Sonyflake;
+
+  constructor({ startTime = new Date() } = {}) {
+    this.sf = new Sonyflake({
+      machineId,
+      startTime,
+    });
+  }
+
+  public next(): bigint {
+    return this.sf.next();
+  }
+}
