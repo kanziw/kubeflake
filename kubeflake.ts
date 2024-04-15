5704dfbbf2674dbe02e5fd0d7bba593007bd4023
Jiwoong Jung <kanziwoong@gmail.com>
2024-04-15 17:43:54 +0900

    Add Cloudflare Workers Compatibility

--- kubeflake.ts
+++ kubeflake.ts
@@ -2,27 +2,36 @@ import { hostname } from 'node:os';
 import { MD5 } from 'crypto-js';
 import { Sonyflake, type SonyflakeIdPayload } from 'sonyflake-js';
 
-const host = hostname();
-const sum = Buffer.from(MD5(host).toString(), 'hex');
-const machineIDBytes = sum.subarray(sum.length - 2); // extract last 2 bytes
-const machineId = BigInt(machineIDBytes.readUInt16BE());
-
-const sf = new Sonyflake({
-  machineId,
-});
-
-export const kubeflake = () => sf.next();
-
+export type KubeflakeOption = {
+  // Default: new Date('2014-09-01T00:00:00Z');
+  // @see https://github.com/sony/sonyflake/blob/fc2f84a086d25d207a91768bc47edfb84972fef5/sonyflake.go#L79
+  startTime?: Date;
+};
 export type KubeflakeIdPayload = SonyflakeIdPayload;
-export const parse = (id: bigint): KubeflakeIdPayload => sf.parse(id);
+
+const getMachineId = (): bigint => {
+  const host = hostname() || globalThis.crypto.randomUUID();
+  const sum = Buffer.from(MD5(host).toString(), 'hex');
+  const machineIDBytes = sum.subarray(sum.length - 2); // extract last 2 bytes
+  return BigInt(machineIDBytes.readUInt16BE(0));
+};
+
+const initSonyflake = (option: KubeflakeOption = {}): Sonyflake =>
+  new Sonyflake({
+    machineId: getMachineId(),
+    startTime: option.startTime,
+  });
+
+export const kubeflake = (option?: KubeflakeOption) => initSonyflake(option).next();
+export const parse = (id: bigint, option?: KubeflakeOption): KubeflakeIdPayload => initSonyflake(option).parse(id);
 
 export class Kubeflake {
   private sf: Sonyflake;
 
-  constructor({ startTime = new Date() } = {}) {
+  constructor(option: KubeflakeOption = {}) {
     this.sf = new Sonyflake({
-      machineId,
-      startTime,
+      machineId: getMachineId(),
+      startTime: option.startTime,
     });
   }
 
